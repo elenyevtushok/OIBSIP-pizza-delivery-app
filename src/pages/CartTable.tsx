8e492e956c18fd1a4a614c675ac367cb9e0679de
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2023-06-07 18:00:40 +0200

    refactor cartTable component

--- src/pages/CartTable.tsx
+++ src/pages/CartTable.tsx
@@ -3,7 +3,6 @@ import { Table, Image, InputNumber } from 'antd';
 import { Order } from '../features/order/dto/Order';
 import { useAppSelector } from '../app/hooks';
 import { selectPizzaById } from '../features/pizzas/pizzasSlice';
-import { Pizza } from '../features/pizzas/dto/Pizza';
 
 
 interface CartTableProps {
@@ -19,51 +18,52 @@ interface PizzaData {
 	onChangeQuantity: (value: number | null) => void;
 }
 
-const columns = [
-	{
-		title: 'Image',
-		dataIndex: 'image',
-		key: 'image',
-	},
-	{
-		title: 'Pizza',
-		dataIndex: 'pizza',
-		key: 'pizza',
-		render: (pizza: string, record: PizzaData) => {
-			return(
-				<div>
-					<p>{pizza}</p>
-					<p>{record.size}</p>
-				</div>
-			)
-		}
-	},
-	{
-		title: 'Quantity',
-		dataIndex: 'amount',
-		key: 'amount',
-		render: (amount: number, record: PizzaData) => {
-			return (
-				<InputNumber
-					min={0}
-					defaultValue={amount}
-					onChange={(value) => record.onChangeQuantity(value)}
-				/>
-			);
-		},
-	},
-	{
-		title: 'Price',
-		dataIndex: 'price',
-		key: 'price',
-		render: (price: number, record: PizzaData) => {
-			const totalPrice = price * record.amount; // Calculate the total price based on quantity
-			return `€ ${totalPrice.toFixed(2)}`; // Display the formatted total price
-		},
-	},
-];
-
 const CartTable: React.FC<CartTableProps> = ({ order }) => {
+
+	const columns = [
+		{
+			title: 'Image',
+			dataIndex: 'image',
+			key: 'image',
+		},
+		{
+			title: 'Pizza',
+			dataIndex: 'pizza',
+			key: 'pizza',
+			render: (pizza: string, record: PizzaData) => {
+				return (
+					<div>
+						<p>{pizza}</p>
+						<p>{record.size}</p>
+					</div>
+				)
+			}
+		},
+		{
+			title: 'Quantity',
+			dataIndex: 'amount',
+			key: 'amount',
+			render: (amount: number, record: PizzaData) => {
+				return (
+					<InputNumber
+						min={0}
+						defaultValue={amount}
+						onChange={(value) => record.onChangeQuantity(value)}
+					/>
+				);
+			},
+		},
+		{
+			title: 'Price',
+			dataIndex: 'price',
+			key: 'price',
+			render: (price: number, record: PizzaData) => {
+				const totalPrice = price * record.amount; // Calculate the total price based on quantity
+				return `€ ${totalPrice.toFixed(2)}`; // Display the formatted total price
+			},
+		},
+	];
+
 	const pizzaData = useAppSelector((state) =>
 		order?.items.map((item) => {
 			const pizza = selectPizzaById(state, item.productId);
